9482115e5021ff8f7e247cf37d6813ce7166c250
Jimesh-patel <vadodariyajimesh1811@gmail.com>
2026-06-11 11:05:40 +0530

    feature(Persistence): Configure databaase connection and marten

--- src/Hosting/ChatApp.Host/Program.cs
+++ src/Hosting/ChatApp.Host/Program.cs
@@ -1,4 +1,11 @@
+
+using Platform.Persistence.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddPersistence(builder.Configuration);
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
